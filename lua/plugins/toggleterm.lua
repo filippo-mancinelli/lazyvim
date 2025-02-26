eb57ea50cd5ecef2e1118bb15a33e9d76cd6b6a0
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2025-02-26 16:45:45 +0100

    more ToggleTerm configuration

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,57 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true,
+  config = function()
+    require("toggleterm").setup({
+      -- Common configuration for all terminals
+      shade_terminals = false,
+      close_on_exit = true,
+      persist_mode = false,
+      start_in_insert = true,
+      float_opts = {
+        border = "curved",
+      },
+    })
+
+    -- Function to set terminal direction
+    local function set_terminal_keymaps()
+      vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+    end
+
+    -- Create an autocmd that will run when the terminal opens
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "term://*toggleterm#*",
+      callback = function()
+        set_terminal_keymaps()
+      end,
+    })
+
+    -- Define the terminals
+    local Terminal = require("toggleterm.terminal").Terminal
+
+    -- Floating terminal (F1)
+    local float_term = Terminal:new({
+      cmd = vim.o.shell,
+      direction = "float",
+      hidden = true,
+      count = 1,
+    })
+
+    -- Left terminal (F2)
+    local term_left = Terminal:new({
+      cmd = vim.o.shell,
+      direction = "horizontal",
+      hidden = true,
+      count = 2,
+    })
+
+    -- Key mappings
+    vim.keymap.set({ "n", "t" }, "<F1>", function()
+      float_term:toggle()
+    end, { noremap = true, silent = true })
+
+    vim.keymap.set({ "n", "t" }, "<F2>", function()
+      term_left:toggle()
+    end, { noremap = true, silent = true })
+  end,
 }
